b1a5e741ffe7914ed5670be1944a5872a6ecd85e
volodymyrlp <mr.volodymyr@egmail.com>
2026-06-28 09:05:51 +0200

    feat: working flask url shortener with mysql

---
+++ app.py
@@ -0,0 +1,57 @@
+from flask import Flask, request, jsonify, redirect
+import random
+import string
+import db
+
+app = Flask(__name__)
+
+
+def generate_code(length=6):
+    return ''.join(random.choices
+                   (string.ascii_letters + string.digits, k=length))
+
+
+@app.route('/health')
+def health():
+    return 'OK'
+
+
+@app.route("/shorten", methods=["POST"])
+def shorten():
+    data = request.get_json()
+    original_url = data.get("url")
+
+    code = generate_code()
+
+    conn = db.get_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+        """
+        INSERT INTO links (short_code, original_url) VALUES (%s, %s)
+        """,
+        (code, original_url)
+    )
+    conn.commit()
+    conn.close()
+
+    return jsonify({"short_code": code}), 201
+
+
+@app.route("/<code>")
+def go_to_url(code):
+    conn = db.get_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+        """
+        SELECT original_url FROM links WHERE short_code = %s
+        """,
+        (code,)
+    )
+    result = cursor.fetchone()
+    conn.close()
+
+    if not result:
+        return jsonify({"error": "URL not found"}), 404
+
+    original_url = result[0]
+    return redirect(original_url)
---
+++ db.py
@@ -0,0 +1,33 @@
+import pymysql
+
+
+def get_connection():
+    connection = pymysql.connect(
+        host='127.0.0.1',
+        port=3307,
+        user="root",
+        password="secret123",
+        database="urlshortener"
+    )
+    return connection
+
+
+def init_db():
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS links (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            short_code VARCHAR(10) NOT NULL,
+            original_url TEXT NOT NULL,
+            clicks INT DEFAULT 0
+        )
+        """)
+    conn.commit()
+    conn.close()
+    print("Everything is ready!")
+
+
+if __name__ == '__main__':
+    init_db()
